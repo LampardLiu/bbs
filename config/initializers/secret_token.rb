cfb2b9a6613be6b921b41b26db7ec764cfb10b6b
Lampard <646007589@qq.com>
2014-11-19 15:08:14 +0800

    bbs-v0.0.0
    无效的，

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bbs::Application.config.secret_key_base = '3a5396041d716144b1c05ee5a6f41ea413f1d7f258318b227f758fbbed6af01116dfadbe0742ef36ae5c4bf089a7778f095ca68939c7c2388a62067df7dfa4a6'
